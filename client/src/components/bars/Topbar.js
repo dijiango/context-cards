ac33e494e24642d38cca573f817450c45056e0b1
Diana Jiang <jiang99diana@gmail.com>
2022-01-27 15:11:47 -0500

    topbar added

---
+++ client/src/components/bars/Topbar.js
@@ -0,0 +1,50 @@
+import { TopbarWrapper, TopbarText, LeftRoutes } from './Topbar.styled';
+import { NavLink } from 'react-router-dom';
+
+const imgStyle = {
+    height: "80px",
+    marginLeft: "-25px"
+}
+
+const textStyle = {
+    color: "white",
+    fontFamily: "Brush Script MT, cursive",
+    fontSize: "15px",
+    fontWeight: "bold",
+    textDecoration: "none",
+}
+
+function Topbar() {
+  return (
+    <div className='topbar'>
+        <TopbarWrapper>
+            <TopbarText>
+                <div className='topbar-left'>
+                    <LeftRoutes>
+                        <NavLink to="/">
+                            <img 
+                            src={require('../images/context.gif')} 
+                            alt='Context' 
+                            style={imgStyle}/>
+                        </NavLink>
+                    </LeftRoutes>
+                    <LeftRoutes>
+                        <NavLink to="/home" style={textStyle}>Home</NavLink>
+                    </LeftRoutes>
+                    <LeftRoutes>
+                        <NavLink to="/create" style={textStyle}>Create</NavLink>
+                    </LeftRoutes>
+                    <LeftRoutes>
+                        <NavLink to="/review" style={textStyle}>Review</NavLink>
+                    </LeftRoutes>
+                </div>
+                <div className='topbar-right' style={textStyle}>
+                    User Profile
+                </div>
+            </TopbarText>
+        </TopbarWrapper>
+    </div>
+
+)}
+
+export default Topbar;
